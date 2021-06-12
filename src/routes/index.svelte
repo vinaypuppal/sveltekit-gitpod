fa4613916596f719a48b969f1bdc2d805148e7a8
vínαч puppαl <me@vinaypuppal.com>
2021-06-12 14:22:19 +0000

    add tailwind

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to Sveltekit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <title>Welcome to SvelteKit</title>
+</svelte:head>
+<main class="p-4">
+  <h1 class="font-bold">Welcome to SvelteKit</h1>
+  <p>
+    Visit <a class="text-[#00f] hover:underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to
+    read the documentation
+  </p>
+</main>
